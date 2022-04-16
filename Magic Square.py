41ef4e8399f537180acc5fc9eee401a70234b0fa
KoduriAjayKumar <93213931+KoduriAjayKumar@users.noreply.github.com>
2022-04-16 11:31:57 +0530

    Magic Square
    In mathematics, a square array of numbers, usually positive integers, is called a magic square if the sums of the numbers in each row, each column, and both main diagonals are the same.[1][2] The order of the magic square is the number of integers along one side (n) should be a odd number.
    The constant sum in every row, column and diagonal are called the magic constant or magic sum, M. The magic constant of a normal magic square depends only on n and has the following value: 
    M = n(n2+1)/2
    
    Conditions :
    1). In any magic square , 1 is located at ( n/2 , n-1) position.
    2). Let's say position of 1 i.e. ( n/2, n-1 ) is ( p, q) then next number which is 2 located at ( p-1, q+1 ) position.
        Anytime if the calculated  row position becomes -1 then makes it n-1 and if column position becomes n they make it 0.
    3). If the calculated position already contains a number then decrement the column by 2 and increment the row by 1.
    4). If anytime row position becomes -1 and column becomes n, switch it to ( 0 , n-2 ).

---
+++ Magic Square.py
@@ -0,0 +1,89 @@
+
+def magic_sqaure(n):
+    MagicSquare = []
+    for i in range(n):
+        L = []
+        for j in range(n):
+            L.append(0)
+        #print(*L) gives the same output of below 5 lines of code
+        MagicSquare.append(L) #Gives an nested list: [[0,0,0],[0,0,0],[0,0,0]]
+   
+    i = n//2
+    j = n -1   # Condition 1 
+    
+    num = n*n
+    count = 1
+    
+    while(count <= num):
+        if (i ==-1 and j == n):
+             i = 0
+             j = n-2  # Condition 4
+        else:
+            if i <0 : # conditon 2 row is becoming -1
+                 i = n-1
+            if j == n: # Condition column value exceeding. 
+                 j = 0
+                   
+        if(MagicSquare[i][j]!=0 ):
+             i = i+1
+             j = j-2   # Codition 3
+             continue  # Continue to check above coditions of while loop from frist without going down.
+        
+        else:
+            MagicSquare[i][j] = count
+            count += 1
+            
+            i = i-1
+            j = j+1
+            
+            
+    for i in range(n):
+        for j in range(n):
+            print(MagicSquare[i][j] , end = " ")
+        print()
+    print()    
+    print("Sum of each row/column/diagonal of above Magic Square(matrix) =", n*(n**2+1)/2)
+    print()
+
+   
+def exit():
+    print("Press 1 to continue \nPress 0 to exit")
+    print()
+    a = int(input())
+    if a == 0 :
+        print("Over")
+    else:
+        k = int(input("please enter a odd number 'n' for the size of matrix n*n :"))
+        print()
+        while (k%2 == 0):
+            print("please enter a 'odd' number.")
+            k = int(input("please enter a odd number 'n' for the size of matrix n*n :"))
+        magic_sqaure(k)          
+        exit()
+        print()
+    
+k = int(input("please enter a odd number 'n' for the size of matrix n*n :"))
+while (k%2 == 0):
+    print("please enter a 'odd' number.")
+    k = int(input("please enter a odd number 'n' for the size of matrix n*n :"))
+print()
+magic_sqaure(k)          
+exit()
+    
+    
+   
+        
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
